2cddf31ca5bb61b6ed39ec56b13c53c057f60489
Donny <tanadornrach@gmail.com>
2024-02-23 21:55:36 +0700

    unfinished

--- election/index.php
+++ election/index.php
@@ -9,10 +9,10 @@
 <body>
     <h1 class="header">LuekTang</h1>
     <div class="buttons-group">
-        <button>แบบสอบถาม</button>
-        <button>นโยบาย</button>
-        <button>พรรคการเมือง</button>
-        <button>แคนดิเดตนายก</button>
+        <button class="button"><a href="questionnaire.php">แบบสอบถาม</a></button>
+        <button class="button"><a href="policies.php">นโยบาย</a></button>
+        <button class="button"><a href="parties.php">พรรคการเมือง</a></button>
+        <button class="button"><a href="candidates.php">แคนดิเดตนายก</a></button>
     </div>
 </body>
 </html>
--- election/policies.php
+++ election/policies.php
@@ -7,7 +7,18 @@
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
-    <h1>นโยบายโดดเด่น</h1>
+    <h1 class="header">นโยบายโดดเด่น</h1>
+    <a class="home-link" href="index.php">
+        <svg
+            class="home-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+        >
+            <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+        </svg>
+    </a>
     <?php
         $servername = "localhost";
         $username = "root";
